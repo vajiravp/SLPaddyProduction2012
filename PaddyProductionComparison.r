756aaa2dd964dc7a472a6639c0164d6248a340d2
vajiravp <45753039+vajiravp@users.noreply.github.com>
2020-06-13 14:48:19 +0530

    Update PaddyProductionComparison.r

--- PaddyProductionComparison.r
+++ PaddyProductionComparison.r
@@ -2,7 +2,7 @@
 # This R code section is for get a comparison graphs of harvested hectares count and average yield received 
 #from a hectair in 2012 paddy production in Sri Lanka
 
-#Below csv formatted data file need to available to run this R code segment
+#Below csv formatted data file need to available as "paddy_production_in_2012.csv" to run this R code segment 
 
 #Season,Sown 000 Acres,Sown 000 Ha,Harvested 000 Acres,Harvested 000 Ha.,Average Yield Bushels/Acre,Average Yield Kg/Ha,Production 000 Bushels,Production 000 Mt.
 #2012 Maha,1734,702,1692,684,86.2,4444,130212,2717
